e69784f377d17e3ea61e4c659143546ad3b9e818
Peter Shen <petershen0307@hotmail.com>
2024-04-23 01:14:21 -0400

    step 2 clone process with namespace

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,6 +1,12 @@
-use std::process::Command;
+use std::{
+    io::stdout,
+    process::{Command, Stdio},
+};
 
-use anyhow::Ok;
+use anyhow::Context;
+use nix::sched;
+use nix::sched::CloneFlags;
+use nix::sys::signal::Signal;
 use tracing::{debug, info, Level};
 
 // prun run <command> <args>
@@ -12,12 +18,29 @@ fn main() -> anyhow::Result<()> {
     anyhow::ensure!(args.len() > 1, "incorrect parameter");
     info!("args len:{}", args.len());
     if args[1].to_lowercase().as_str() == "run" {
-        let args2 = args[3..].to_vec();
-        debug!("{:?}", args2);
-        Command::new(&args[2])
-            .args(&args2)
-            .spawn()
-            .expect("failed to run command");
+        const STACK_SIZE: usize = 1024 * 1024;
+        let stack: &mut [u8; STACK_SIZE] = &mut [0; STACK_SIZE];
+        unsafe {
+            sched::clone(
+                Box::new(|| -> isize {
+                    let args2 = args[3..].to_vec();
+                    debug!("{:?}", args2);
+                    let mut child = Command::new(&args[2])
+                        .args(&args2)
+                        .spawn()
+                        .expect("failed to run command");
+                    child.wait().expect("failed to wait child output");
+                    0
+                }),
+                stack,
+                CloneFlags::CLONE_NEWUSER
+                    | CloneFlags::CLONE_NEWUTS
+                    | CloneFlags::CLONE_NEWPID
+                    | CloneFlags::CLONE_NEWNS,
+                Some(Signal::SIGCHLD as i32),
+            )
+            .expect("can't run process");
+        }
     } else {
         anyhow::bail!("invalid command");
     }
